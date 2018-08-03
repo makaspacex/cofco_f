3a59f51fa8e416116addaef6f2ea438e58d982ea
xiahuo <1358384211@qq.com>
2018-08-03 19:28:13 +0800

    修改字符错误

--- app/cofco/view/spider/task_form.php
+++ app/cofco/view/spider/task_form.php
@@ -28,7 +28,7 @@ $name=$user[ADMIN_ID];
         <label class="layui-form-label">更新频率</label>
         <div class="layui-input-inline">
             <select name="frequen"  class="field-frequen" type="select" >
-                <option value ="00"selected>按天更新（请选择）</option>
+                <option value ="00"selected>更新频率（请选择）</option>
                 <option value ="0"> 只更新一次</option>
                 <option value ="1">一天更新一次</option>
                 <option value="2">两天更新一次</option>
