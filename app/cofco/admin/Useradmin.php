663d9fea86141bfaf1ebb45b561b72350f82e3a7
LiFH <fh.li@foxmail.com>
2019-05-14 16:22:38 +0800

    fixed:用户管理数据库名

--- app/cofco/admin/Useradmin.php
+++ app/cofco/admin/Useradmin.php
@@ -10,27 +10,6 @@ use app\cofco\model\AdminUserlog as LogModel;
  */
 class Useradmin extends AdminBase
 {
-    public $tab_data = [];
-    /**
-     * 初始化方法
-     */
-    protected function _initialize()
-    {
-        parent::_initialize();
-
-        $tab_data['menu'] = [
-            [
-                'title' => '数据统计',
-                'url' => 'cofco/useradmin/index',
-            ],
-            [
-                'title' => '人员信息管理',
-                'url' => 'admin/user/index',
-            ],
-        ];
-        $this->tab_data = $tab_data;
-    }
-
     /**
      * 数据统计
      * @author LiFH
@@ -39,15 +18,13 @@ class Useradmin extends AdminBase
     public function index()
     {
         $uID = $_SESSION['hisiphp_']['admin_user']['uid'];
-
         $sql = 'select year,month, 
             sum(case when type like 1 then 1 else 0 end) as "create",
             sum(case when type like 2 then 1 else 0 end) as "pre",
             sum(case when type like 3 then 1 else 0 end) as "finaly"
-            FROM `hisi_admin_userlog`  
+            FROM `cofco_admin_userlog`  
             WHERE uID like '.$uID.'
             group by year,month';
-        // $data_list = LogModel::where($sqlmap)->group('year')->group('month')->select();
         $data_list = LogModel::query($sql);
         $this->assign('data_list', $data_list);
         return $this->fetch();
--- app/cofco/model/AdminUserlog.php
+++ app/cofco/model/AdminUserlog.php
@@ -8,7 +8,7 @@ use think\Model;
  * 日志记录
  * @package app\admin\model
  */
-class AdminUserlog extends Model
+class AdminUserlog extends AdminBase
 {
     // 定义时间戳字段名
     protected $createTime = 'time';
