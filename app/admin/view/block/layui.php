e08f96212aede52bcdef050b11501e55e78306f2
izhangxm <izhangxm@foxmail.com>
2019-05-14 15:45:53 +0800

    更新版本号; 为静态资源增加版本号，防止缓存

--- app/admin/view/block/layui.php
+++ app/admin/view/block/layui.php
@@ -1,11 +1,11 @@
 {if condition="defined('MODULENAME') && MODULENAME eq 'COFCO' "}
     {if condition="$layout_on eq false"}
-        <link rel="stylesheet" href="__ADMIN_JS__/layui-v2.4.5/css/layui.css"  >
-        <link rel="stylesheet" href="__ADMIN_JS__/layui-v2.4.5/tablePlug.css" >
+        <link rel="stylesheet" href="__ADMIN_JS__/layui-v2.4.5/css/layui.css?v={:config('hisiphp.version')}"  >
+        <link rel="stylesheet" href="__ADMIN_JS__/layui-v2.4.5/tablePlug.css?v={:config('hisiphp.version')}" >
         <link rel="stylesheet" href="__ADMIN_CSS__/project.css?v={:config('hisiphp.version')}">
     {/if}
-<script src="__ADMIN_JS__/layui-v2.4.5/layui.js"></script>
-<script src="__ADMIN_JS__/jquery.2.1.4.min.js"></script>
+<script src="__ADMIN_JS__/layui-v2.4.5/layui.js?v={:config('hisiphp.version')}"></script>
+<script src="__ADMIN_JS__/jquery.2.1.4.min.js?v={:config('hisiphp.version')}"></script>
 <script>
     var ADMIN_PATH = "{$_SERVER['SCRIPT_NAME']}", LAYUI_OFFSET = 0;
     layui.config({
@@ -19,7 +19,7 @@
         <link rel="stylesheet" href="__ADMIN_CSS__/project.css?v={:config('hisiphp.version')}">
     {/if}
 <script src="__ADMIN_JS__/layui/layui.js?v={:config('hisiphp.version')}"></script>
-<script src="__ADMIN_JS__/jquery.2.1.4.min.js"></script>
+<script src="__ADMIN_JS__/jquery.2.1.4.min.js?v={:config('hisiphp.version')}"></script>
 <script>
     var ADMIN_PATH = "{$_SERVER['SCRIPT_NAME']}", LAYUI_OFFSET = 0;
     layui.config({
--- version.php
+++ version.php
@@ -2,9 +2,9 @@
 return [
   'hisiphp' => 
   [
-    'name' => 'HisiPHP',
-    'version' => '1.0.3',
-    'release' => 20180325,
+    'name' => 'CofcoDataA',
+    'version' => '2.0.4',
+    'release' => 20190514,
     'copyright' => 'HisiPHP.com',
     'url' => 'http://www.hisiphp.com',
   ],
