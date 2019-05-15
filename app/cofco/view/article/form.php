d12253ba59a9675a88fd7bf5def54230b7f6fe65
LiFH <fh.li@foxmail.com>
2019-05-15 15:00:39 +0800

    fixed:修复编辑后表格刷新问题

--- app/cofco/view/article/form.php
+++ app/cofco/view/article/form.php
@@ -1,19 +1,18 @@
 {include file="cofco@block/edit_form"/}
 <script>
     var formData = {:json_encode($data_info)};
-    var pre_status = formData['status'];
-    console.log(pre_status);
-    var text1='';var text2='';
+    var pre_status = formData['status'];   // 更改之前的状态
+    var text1 = '';
+    var text2 = '';
     function func(data) {
         var $ = layui.jquery;
-        for(var i=0;i<data.length;i++){
-            if (i==0){
-                text1= data[i]['name'];
-                text2= data[i]['title'];
-            }
-            else{
-                text1=text1+'#'+data[i]['name'];
-                text2=text2+'#'+data[i]['title'];
+        for (var i = 0; i < data.length; i++) {
+            if (i == 0) {
+                text1 = data[i]['name'];
+                text2 = data[i]['title'];
+            } else {
+                text1 = text1 + '#' + data[i]['name'];
+                text2 = text2 + '#' + data[i]['title'];
             }
         }
         $('input[name="tag_id"]').val(text1);
@@ -27,27 +26,30 @@
         var form_s = $('#editForm');
         var form_data = form_s.serializeArray();
         form_data.push({
-            name:  "pre_status",
+            name: "pre_status",
             value: pre_status
         });
-        console.log(form_data);
+        // console.log(form_data);
         var article_api_url_edit = "{$article_api_url}/edit";
-        $.post(article_api_url_edit,form_data,function (res) {
-            console.log(res);
-            if(res.code===0){
+        $.post(article_api_url_edit, form_data, function (res) {
+            if (res.code === 0) {
                 layer.msg(
                     res.message
-                    ,{time:1000}
-                    ,function () {
-                    var index = parent.layer.getFrameIndex(window.name);
-                    parent.layer.close(index);//关闭当前页
-                });
+                    , {time: 1000}
+                    , function () {
+                        var index = parent.layer.getFrameIndex(window.name);
+                        parent.layer.close(index);//关闭当前页
+                        parent.layui.table.reload('articletable'); //刷新表格
+                    });
 
-            }
-            else {
+            } else {
                 layer.msg(res.message);
             }
         });
-        if(e.preventDefault){ e.preventDefault(); }else{ window.event.returnValue == false;}
+        if (e.preventDefault) {
+            e.preventDefault();
+        } else {
+            window.event.returnValue == false;
+        }
     });
 </script>
