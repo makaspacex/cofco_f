e6ecdf0c96b598666a4030282981e72c03ca6b9c
izhangxm <izhangxm@foxmail.com>
2019-05-19 14:39:11 +0800

    实时日志查看

--- application/cofco/admin/Journal.php
+++ application/cofco/admin/Journal.php
@@ -3,7 +3,10 @@
 namespace app\cofco\admin;
 
 use app\cofco\model\AdminKw as KwModel;
+use app\cofco\model\AdminJournal;
 use think\Config;
+use app\cofco\model\AdminPending as PendingModel;
+use think\db\Where;
 
 
 class Journal extends AdminBase
@@ -12,6 +15,19 @@ class Journal extends AdminBase
     public function index()
     {
         return $this->fetch();
+    }
+
+    public function sync_info(){
+        try {
+            $res = AdminJournal::all();
+            foreach ($res as $key=>$row){
+                $data = ['impact_factor'=>$row['impact_factor'],'journal_zone'=>$row['journal_zone']];
+                PendingModel::where(new Where(['issnl|issne|issnp'=>['LIKE', '%' . $row['issn'] . '%']]))->update($data);
+            }
+            return json(['code' => 0, 'message' => '操作完成']);
+        } catch (\Exception $e) {
+            return json(['code' => 25, 'message' => '操作失败' . $e->getMessage()]);
+        }
     }
 
 }
